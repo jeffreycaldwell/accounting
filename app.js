99e82b062efe5048324a8571009fb8ffbbd2b7b2
Jeffrey <woundedkarma@gmail.com>
2018-03-08 07:11:35 +0000

    making progress, initial stuff, some views

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// start
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+   res.render("landing"); 
+});
+
+app.get("/companies", function(req, res){
+    
+    var companies = [
+        {name: "Boomer LLC", balance: "$234,000.98"},
+        {name: "Floatilla Hun, Inc.", balance: "$293,883.03"},
+        {name: "DigiDoIt", balance: "$384,230.34" }
+        ]
+        
+   res.render("companies", {companies: companies});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Account server has started.");
+});
